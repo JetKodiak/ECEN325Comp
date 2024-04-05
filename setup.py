d6d69e0899dc555c055935b56c065209fc297a18
JetKodiak <118746030+JetKodiak@users.noreply.github.com>
2024-04-05 10:47:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
-setup(name='Compt_325',
+setup(name='ECEN325Comp',
       version='0.0.1',
       description='Adds simulators for 325 components',
       url='github.com/JetKodiak/ECEN325Comp',
       author='Hyperion',
       author_email='',
       license='Free to use',
-      packages=['Compt_325'],
+      packages=['ECEN325Comp'],
       zip_safe=False)
